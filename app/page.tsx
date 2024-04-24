1c48bc826204f02590c74f6fb20e6a19db5ac6af
EmmaDjams <djambemmanuel@gmail.com>
2024-04-24 14:02:03 +0800

    implementing clerk webhooks

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
+  const a=process.env.WEBHOOK_SECRET
   return (
     <div className="">
       
